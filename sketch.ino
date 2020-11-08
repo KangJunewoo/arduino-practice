445890f4b92a5050dfc4cf197877925296c5e516
KangJunewoo <brianjune.dev@gmail.com>
2020-11-08 15:58:02 +0900

    [REFACTOR] switch-basic

--- sketch.ino
+++ sketch.ino
@@ -1,43 +1,11 @@
-/**
- * pwm을 활용해
- * 아날로그처럼 빛의 밝기를 조절할 수 있다.
-*/
-
-const int RLED = 9;
-const int GLED = 10;
-const int BLED = 11;
+const int LED = 9;
+const int BUTTON = 2;
 
 void setup() {
-  pinMode(RLED, OUTPUT);
-  pinMode(GLED, OUTPUT);
-  pinMode(BLED, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
-  int i;
-  for(i=0; i<256; i++){
-    analogWrite(RLED, i);
-    delay(10);
-  }
-  for(i=0; i<256; i++){
-    analogWrite(GLED, i);
-    delay(10);
-  }
-  for(i=0; i<256; i++){
-    analogWrite(BLED, i);
-    delay(10);
-  }
-
-  for(i=255; i>=0 ; i--){
-    analogWrite(RLED, i);
-    delay(10);
-  }
-  for(i=255; i>=0 ; i--){
-    analogWrite(GLED, i);
-    delay(10);
-  }
-  for(i=255; i>=0 ; i--){
-    analogWrite(BLED, i);
-    delay(10);
-  }
+  digitalWrite(LED, digitalRead(BUTTON));
 }
